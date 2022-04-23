92d325ff8b948acfb8326b7097514012ca1ec258
Vladimir21500 <vova.datsiuk.20011@gmail.com>
2022-04-23 12:43:10 +0300

    repeat learn

---
+++ lesson1_1/task1/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Header from './Header';
+import { Context } from './context';
+
+class App extends React.Component {
+  state = {
+    userData: { name: 'Nikola Tesla', avatar_url: 'https://avatars3.githubusercontent.com/u10001' },
+  };
+
+  render() {
+    return (
+      <div className="page">
+        <Context.Provider value={this.state.userData}>
+          <Header />
+        </Context.Provider>
+      </div>
+    );
+  }
+}
+
+export default App;
